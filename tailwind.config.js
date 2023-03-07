d2dd48d44dc7fd32a2fc2299f31089f8472c4e29
Jessica Villa <suhkha92@gmail.com>
2023-03-06 18:39:30 -0600

    basic markup for ad platform

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./public/ads.html",
+    "./public/detail.html",
+    "./src/*.{html,js}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
